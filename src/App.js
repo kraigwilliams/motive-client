59a7dbda7f34bf38e1b4fbfd9d2f3dd3e972dc47
Jordan Castillo <jordanxcallaway@gmail.com>
2020-03-24 08:33:51 -0700

    commented out component instances in app.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,51 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
-import NavBar from './components/NavBar/NavBar'
-import LandingPage from './components/LandingPage/LandingPage'
-import SignUp from './components/SignUp/SignUp'
-import Login from './components/Login/Login'
-import Dashboard from './components/Dashboard/Dashboard'
-import GroupsPage from './components/GroupsPage/GroupsPage'
-import NotFoundPage from './components/Utils/NotFoundPage'
-import './App.css';
+// import NavBar from './components/NavBar/NavBar'
+// import LandingPage from './components/LandingPage/LandingPage'
+// import SignUp from './components/SignUp/SignUp'
+// import Login from './components/Login/Login'
+// import Dashboard from './components/Dashboard/Dashboard'
+// import GroupsPage from './components/GroupsPage/GroupsPage'
+// import NotFoundPage from './components/Utils/NotFoundPage'
+// import './App.css';
 
 function App() {
   return (
-    <main className="root">
-      <NavBar/>
-      
+    <>
+      {/* <NavBar/> */}
+      <main className="app">
       <Switch>
         <Route
-          to='/'
-          component={LandingPage}
+          path='/'
+          // component={LandingPage}
         />
 
         <Route
-          to='/signup'
-          component={SignUp}
+          path='/signup'
+          // component={SignUp}
         />
 
         <Route 
-          to='/login'
-          component={Login}
+          path='/login'
+          // component={Login}
         />
 
         <Route 
-          to='/dashboard'
-          component={Dashboard}
+          path='/dashboard'
+          // component={Dashboard}
         />
        
         <Route 
-          to='/groups'
-          component={GroupsPage}
+          path='/groups'
+          // component={GroupsPage}
         />
 
         <Route
-          component={NotFoundPage}
+          // component={NotFoundPage}
         />
       </Switch>
     </main>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter }from 'react-router-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+// import './index.css';
+// import * as serviceWorker from './serviceWorker'
 
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
+
+// serviceWorker.unregister()
