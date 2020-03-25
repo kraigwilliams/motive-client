d500fec50679b67f266abffc4ffc19db5dc77d0c
Jordan Castillo <jordanxcallaway@gmail.com>
2020-03-25 10:50:04 -0700

    implemented user context file in app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './contexts/UserContext'
 import App from './App';
 import './index.css';
 // import * as serviceWorker from './serviceWorker'
@@ -8,7 +9,9 @@ import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </BrowserRouter>
   ,
   document.getElementById('root')
--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -15,7 +15,34 @@ const AuthApiService = {
           ? res.json().then(e => Promise.reject(e)) 
           : res.json()
       )
-  }
+  },
+  postLogin({ username, password }) {
+    return fetch(`${config.API_ENDPOINT}/auth/token`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(err => Promise.reject(err))
+          : res.json()
+      )
+  },
+  refreshToken() {
+    return fetch(`${config.API_ENDPOINT}/auth/token`, {
+      method: 'PUT',
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
 }
 
 export default AuthApiService
