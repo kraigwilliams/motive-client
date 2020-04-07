02059dc9ba099788c90ce651a5c21144ba0fcd2e
Scott Lingner <slingner@gmail.com>
2020-04-07 16:27:55 -0700

    add spinner to login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,6 +10,8 @@ import {
 } from "../Form/Form";
 import AuthApiService from "../../services/auth-api-service";
 import { FormButton, GoBack } from "../Button/Button";
+import Loader from "react-loader-spinner";
+import { colors } from "../constants";
 
 export default class Login extends Component {
   static contextType = UserContext;
@@ -20,7 +22,7 @@ export default class Login extends Component {
     },
   };
 
-  state = { error: null };
+  state = { error: null, loading: false };
 
   firstInput = React.createRef();
 
@@ -44,6 +46,7 @@ export default class Login extends Component {
         username.value = "";
         password.value = "";
         this.context.processLogin(res.authToken);
+        this.setState({ loading: true });
         this.handleLoginSuccess();
       })
 
@@ -63,55 +66,76 @@ export default class Login extends Component {
   }
 
   render() {
-    const { error } = this.state;
+    const { error, loading } = this.state;
     return (
-      <LoginWrapper>
-        <FormWrapper
-          onSubmit={this.handleSubmit}
-          onChange={this.handleChange.bind(this)}
-        >
-          <div style={{ height: "30px" }}>
-            <GoBack
-              type="reset"
-              onClick={() => this.props.history.push("/")}
-              margin="10px 0px 20px 0px"
+      <>
+        {loading ? (
+          <LoginWrapper style={{ display: "flex" }}>
+            <Loader
+              type="TailSpin"
+              color={colors.slategrey}
+              height={80}
+              width={80}
+              style={{ marginLeft: "auto", marginRight: "auto" }}
             />
-          </div>
-          <FormTitle>Login</FormTitle>
-          {error && (
-            <p style={{ textAlign: "center", marginTop: "10px" }}>{error}</p>
-          )}
+          </LoginWrapper>
+        ) : (
+          <LoginWrapper>
+            <FormWrapper
+              onSubmit={this.handleSubmit}
+              onChange={this.handleChange.bind(this)}
+            >
+              <div style={{ height: "30px" }}>
+                <GoBack
+                  type="reset"
+                  onClick={() => this.props.history.push("/")}
+                  margin="10px 0px 20px 0px"
+                />
+              </div>
+              <FormTitle>Login</FormTitle>
+              {error && (
+                <p
+                  style={{
+                    textAlign: "center",
+                    marginTop: "10px",
+                  }}
+                >
+                  {error}
+                </p>
+              )}
 
-          <FormLabel htmlFor="login-username-input">
-            Username <Required />
-          </FormLabel>
-          <FormInput
-            placeholder="henry_thoreau"
-            ref={this.firstInput}
-            id="login-username-input"
-            name="username"
-            aria-label="Enter your username"
-            aria-required="true"
-            required
-          />
+              <FormLabel htmlFor="login-username-input">
+                Username <Required />
+              </FormLabel>
+              <FormInput
+                placeholder="henry_thoreau"
+                ref={this.firstInput}
+                id="login-username-input"
+                name="username"
+                aria-label="Enter your username"
+                aria-required="true"
+                required
+              />
 
-          <FormLabel htmlFor="login-password-input">
-            Password <Required />
-          </FormLabel>
+              <FormLabel htmlFor="login-password-input">
+                Password <Required />
+              </FormLabel>
 
-          <FormInput
-            type="password"
-            placeholder="SimpleLiving1"
-            id="login-password-input"
-            name="password"
-            aria-label="Enter your password"
-            aria-required="true"
-            required
-          />
+              <FormInput
+                type="password"
+                placeholder="SimpleLiving1"
+                id="login-password-input"
+                name="password"
+                aria-label="Enter your password"
+                aria-required="true"
+                required
+              />
 
-          <FormButton type="submit">Login</FormButton>
-        </FormWrapper>
-      </LoginWrapper>
+              <FormButton type="submit">Login</FormButton>
+            </FormWrapper>
+          </LoginWrapper>
+        )}
+      </>
     );
   }
 }
